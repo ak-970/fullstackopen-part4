4b48ca6b7036edd416e26185d6db7ed93db2e082
ak-970 <antje.kasemann@gmail.com>
2023-10-03 14:26:40 +0200

    Part 4: Project structure

---
+++ utils/middleware.js
@@ -0,0 +1,43 @@
+const morgan = require('morgan')
+const logger = require('./logger')
+
+const requestLogger = morgan(function(tokens, request, response) {
+  return [
+    tokens.method(request, response),
+    tokens.url(request, response),
+    tokens.status(request, response),
+    tokens.res(request, response, 'content-length'),
+    '-',
+    tokens['response-time'](request, response), 'ms',
+    JSON.stringify(request.body)
+  ].join(' ')
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({
+    error: 'unknown endpoint'
+  })
+}
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      error: error.message
+    })
+  }
+
+  next(error)
+}
+
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
